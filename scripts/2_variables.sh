3579e9a23a36c7060125bb4ab224a6f7cd30752f
Joshua Stein <jstein@Joshuas-MacBook-Pro.local>
2023-01-10 14:50:20 -0500

    updates

--- scripts/2_variables.sh
+++ scripts/2_variables.sh
@@ -1,11 +1,6 @@
 STACK_NAMES=$(eksctl get nodegroup --cluster eksworkshop-eksctl -o json | jq -r '.[].StackName')
 echo "STACK_NAMES: $STACK_NAMES"
-#STACK_NAME=$STACK_NAMES | head -n 1
-#ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::EC2::Instance") | .PhysicalResourceId')
+STACK_NAME=$STACK_NAMES | head -n 1
+ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::EC2::Instance") | .PhysicalResourceId')
 
-ROLE_NAME=""eksctl-eksworkshop-eksctl-nodegro-NodeInstanceRole-QQ3S5ZDYDZ2F""
 echo "export ROLE_NAME=${ROLE_NAME}" | tee -a ~/.zprofile
-
-export EKS_KMS_ARN=$(aws kms describe-key --key-id alias/EKS --query KeyMetadata.Arn --output text)
-export ECR_KMS_ARN=$(aws kms describe-key --key-id alias/ECR --query KeyMetadata.Arn --output text)
-
--- scripts/k8_dashboard.sh
+++ scripts/k8_dashboard.sh
@@ -1,1 +1,2 @@
+#After installing argo you can use it to install the kubernetes dashboard
 argocd app create k8-dashboard --repo https://github.com/jstein-vr/k8-dashboard.git  --dest-server https://kubernetes.default.svc --dest-namespace kubernetes-dashboard --path ./
